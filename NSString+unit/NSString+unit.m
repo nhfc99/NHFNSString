2ce731ecfd7d14d681130474d0595549a79e482c
nhfc99 <nhfc99@163.com>
2016-11-21 09:30:14 +0800

    更新兼容177号段

--- NSString+unit/NSString+unit.m
+++ NSString+unit/NSString+unit.m
@@ -21,7 +21,7 @@
     NSString * MOBILE = @"^1(3[0-9]|5[0-35-9]|8[025-9])\\d{8}$";
     NSString * CM = @"^1(34[0-8]|(3[5-9]|5[017-9]|8[278])\\d)\\d{7}$";
     NSString * CU = @"^1(3[0-2]|5[256]|8[56])\\d{8}$";
-    NSString * CT = @"^1((33|53|8[09])[0-9]|349)\\d{7}$";
+    NSString * CT = @"^1((33|53|8[09]|77)[0-9]|349)\\d{7}$";
     // NSString * PHS = @"^0(10|2[0-5789]|\\d{3})\\d{7,8}$";
     
     NSPredicate *regextestmobile = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", MOBILE];
